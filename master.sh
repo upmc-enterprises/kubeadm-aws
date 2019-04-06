68eb31748a0abdca1f16e27ba60b808b9eaf8114
Vilmos Nebehaj <v.nebehaj@gmail.com>
2019-04-05 18:22:19 -0700

    Use kube-dns

--- master.sh
+++ master.sh
@@ -11,11 +11,34 @@ systemctl start docker
 
 sysctl net.bridge.bridge-nf-call-iptables=1
 
-kubeadm init --token=${k8stoken} --pod-network-cidr=172.20.0.0/16
+cat <<EOF > /tmp/kubeadm-config.yaml
+apiVersion: kubeadm.k8s.io/v1beta1
+kind: InitConfiguration
+bootstrapTokens:
+- groups:
+  - system:bootstrappers:kubeadm:default-node-token
+  token: ${k8stoken}
+---
+apiVersion: kubeadm.k8s.io/v1beta1
+kind: ClusterConfiguration
+dns:
+  type: kube-dns
+networking:
+  podSubnet: 172.20.0.0/16
+  serviceSubnet: 10.96.0.0/12
+EOF
+kubeadm init --config=/tmp/kubeadm-config.yaml
 
 echo 'KUBELET_KUBEADM_ARGS=--cgroup-driver=cgroupfs --pod-infra-container-image=k8s.gcr.io/pause:3.1' > /var/lib/kubelet/kubeadm-flags.env
 systemctl restart kubelet
 
+export KUBECONFIG=/etc/kubernetes/admin.conf
+
+# Configure kubectl.
 mkdir -p /home/ubuntu/.kube
-sudo cp -i /etc/kubernetes/admin.conf /home/ubuntu/.kube/config
+sudo cp -i $KUBECONFIG /home/ubuntu/.kube/config
 sudo chown ubuntu: /home/ubuntu/.kube/config
+
+kubectl get cm -n kube-system kube-proxy -oyaml | sed -r '/^\s+resourceVersion:/d' | sed 's/masqueradeAll: false/masqueradeAll: true/' | kubectl replace -f -
+
+kubectl patch -n kube-system deployment kube-dns --patch '{"spec": {"template": {"spec": {"tolerations": [{"key": "CriticalAddonsOnly", "operator": "Exists"}]}}}}'
