2e334dc5048577e438ae894cd4db4b4a4b6ad2b4
Vilmos Nebehaj <v.nebehaj@gmail.com>
2019-05-13 13:24:28 -0700

    Fix indentation

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-
 /*
 Copyright (c) 2016, UPMC Enterprises
 All rights reserved.
@@ -29,74 +28,74 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_hostnames = true
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
 
-    tags {
-        Name = "TF_VPC"
-    }
+  tags {
+    Name = "TF_VPC"
+  }
 
-    provisioner "local-exec" {
-        # Remove any leftover instance, security group etc Milpa created. They
-        # would prevent terraform from destroying the VPC.
-        when    = "destroy"
-        command = "./cleanup-vpc.sh ${self.id} ${var.cluster-name}"
-        interpreter = ["/bin/bash", "-c"]
-        environment = {
-            AWS_ACCESS_KEY_ID = "${var.aws-access-key-id}"
-            AWS_SECRET_ACCESS_KEY = "${var.aws-secret-access-key}"
-        }
+  provisioner "local-exec" {
+    # Remove any leftover instance, security group etc Milpa created. They
+    # would prevent terraform from destroying the VPC.
+    when    = "destroy"
+    command = "./cleanup-vpc.sh ${self.id} ${var.cluster-name}"
+    interpreter = ["/bin/bash", "-c"]
+    environment = {
+      AWS_ACCESS_KEY_ID = "${var.aws-access-key-id}"
+      AWS_SECRET_ACCESS_KEY = "${var.aws-secret-access-key}"
     }
+  }
 }
 
 resource "aws_internet_gateway" "gw" {
-    vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.main.id}"
 
-    tags {
-        Name = "TF_main"
-    }
+  tags {
+    Name = "TF_main"
+  }
 
-    provisioner "local-exec" {
-        # Remove any leftover instance, security group etc Milpa created. They
-        # would prevent terraform from destroying the VPC.
-        when    = "destroy"
-        command = "./cleanup-vpc.sh ${self.vpc_id} ${var.cluster-name}"
-        interpreter = ["/bin/bash", "-c"]
-        environment = {
-            AWS_ACCESS_KEY_ID = "${var.aws-access-key-id}"
-            AWS_SECRET_ACCESS_KEY = "${var.aws-secret-access-key}"
-        }
+  provisioner "local-exec" {
+    # Remove any leftover instance, security group etc Milpa created. They
+    # would prevent terraform from destroying the VPC.
+    when    = "destroy"
+    command = "./cleanup-vpc.sh ${self.vpc_id} ${var.cluster-name}"
+    interpreter = ["/bin/bash", "-c"]
+    environment = {
+      AWS_ACCESS_KEY_ID = "${var.aws-access-key-id}"
+      AWS_SECRET_ACCESS_KEY = "${var.aws-secret-access-key}"
     }
+  }
 }
 
 resource "aws_route_table" "r" {
-    vpc_id = "${aws_vpc.main.id}"
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = "${aws_internet_gateway.gw.id}"
-    }
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
 
-    depends_on = ["aws_internet_gateway.gw"]
+  depends_on = ["aws_internet_gateway.gw"]
 
-    tags {
-        Name = "TF_main"
-    }
+  tags {
+    Name = "TF_main"
+  }
 }
 
 resource "aws_route_table_association" "publicA" {
-    subnet_id = "${aws_subnet.publicA.id}"
-    route_table_id = "${aws_route_table.r.id}"
+  subnet_id = "${aws_subnet.publicA.id}"
+  route_table_id = "${aws_route_table.r.id}"
 }
 
 resource "aws_subnet" "publicA" {
-    vpc_id = "${aws_vpc.main.id}"
-    cidr_block = "10.0.100.0/24"
-    availability_zone = "us-east-1c"
-    map_public_ip_on_launch = true
+  vpc_id = "${aws_vpc.main.id}"
+  cidr_block = "10.0.100.0/24"
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = true
 
-    tags {
-        Name = "TF_PubSubnetA"
-    }
+  tags {
+    Name = "TF_PubSubnetA"
+  }
 }
 
 resource "aws_security_group" "kubernetes" {
@@ -105,25 +104,25 @@ resource "aws_security_group" "kubernetes" {
   vpc_id = "${aws_vpc.main.id}"
 
   ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["10.0.0.0/16"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
 
   egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags {
@@ -140,20 +139,20 @@ data "template_file" "master-userdata" {
 }
 
 data "template_file" "worker-userdata" {
-    template = "${file("${var.worker-userdata}")}"
+  template = "${file("${var.worker-userdata}")}"
 
-    vars {
-        k8stoken = "${var.k8stoken}"
-        masterIP = "${aws_instance.k8s-master.private_ip}"
-        cluster_name = "${var.cluster-name}"
-        aws_access_key_id = "${var.aws-access-key-id}"
-        aws_secret_access_key = "${var.aws-secret-access-key}"
-        ssh_key_name = "${var.ssh-key-name}"
-        license_key = "${var.license-key}"
-        license_id = "${var.license-id}"
-        license_username = "${var.license-username}"
-        license_password = "${var.license-password}"
-    }
+  vars {
+    k8stoken = "${var.k8stoken}"
+    masterIP = "${aws_instance.k8s-master.private_ip}"
+    cluster_name = "${var.cluster-name}"
+    aws_access_key_id = "${var.aws-access-key-id}"
+    aws_secret_access_key = "${var.aws-secret-access-key}"
+    ssh_key_name = "${var.ssh-key-name}"
+    license_key = "${var.license-key}"
+    license_id = "${var.license-id}"
+    license_username = "${var.license-username}"
+    license_password = "${var.license-password}"
+  }
 }
 
 resource "aws_instance" "k8s-master" {
@@ -168,7 +167,7 @@ resource "aws_instance" "k8s-master" {
   depends_on = ["aws_internet_gateway.gw"]
 
   tags {
-      Name = "k8s-master"
+    Name = "k8s-master"
   }
 }
 
@@ -180,10 +179,11 @@ resource "aws_instance" "k8s-worker" {
   key_name = "${var.ssh-key-name}"
   associate_public_ip_address = true
   vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
+  iam_instance_profile = "${aws_iam_instance_profile.k8s-milpa.id}"
 
   depends_on = ["aws_internet_gateway.gw"]
 
   tags {
-      Name = "k8s-worker"
+    Name = "k8s-worker"
   }
 }
