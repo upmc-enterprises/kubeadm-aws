1cd6d07324805dbfa0e43ea9ba06850474efef98
Vilmos Nebehaj <v.nebehaj@gmail.com>
2019-06-05 10:46:24 -0700

    Add separate IAM role for k8s master

--- main.tf
+++ main.tf
@@ -127,8 +127,8 @@ resource "aws_security_group" "kubernetes" {
   tags = "${local.k8s_cluster_tags}"
 }
 
-resource "aws_iam_role" "k8s-milpa" {
-  name = "k8s-milpa-${var.cluster-name}"
+resource "aws_iam_role" "k8s-master" {
+  name = "k8s-master-${var.cluster-name}"
   assume_role_policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -145,9 +145,106 @@ resource "aws_iam_role" "k8s-milpa" {
 EOF
 }
 
-resource "aws_iam_role_policy" "k8s-milpa" {
-  name = "k8s-milpa-${var.cluster-name}"
-  role = "${aws_iam_role.k8s-milpa.id}"
+resource "aws_iam_role_policy" "k8s-master" {
+  name = "k8s-master-${var.cluster-name}"
+  role = "${aws_iam_role.k8s-master.id}"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "autoscaling:DescribeAutoScalingGroups",
+        "autoscaling:DescribeLaunchConfigurations",
+        "autoscaling:DescribeTags",
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions",
+        "ec2:DescribeRouteTables",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeVolumes",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifyVolume",
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CreateRoute",
+        "ec2:DeleteRoute",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteVolume",
+        "ec2:DetachVolume",
+        "ec2:RevokeSecurityGroupIngress",
+        "ec2:DescribeVpcs",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:AttachLoadBalancerToSubnets",
+        "elasticloadbalancing:ApplySecurityGroupsToLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancer",
+        "elasticloadbalancing:CreateLoadBalancerPolicy",
+        "elasticloadbalancing:CreateLoadBalancerListeners",
+        "elasticloadbalancing:ConfigureHealthCheck",
+        "elasticloadbalancing:DeleteLoadBalancer",
+        "elasticloadbalancing:DeleteLoadBalancerListeners",
+        "elasticloadbalancing:DescribeLoadBalancers",
+        "elasticloadbalancing:DescribeLoadBalancerAttributes",
+        "elasticloadbalancing:DetachLoadBalancerFromSubnets",
+        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
+        "elasticloadbalancing:ModifyLoadBalancerAttributes",
+        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
+        "elasticloadbalancing:SetLoadBalancerPoliciesForBackendServer",
+        "elasticloadbalancing:AddTags",
+        "elasticloadbalancing:CreateListener",
+        "elasticloadbalancing:CreateTargetGroup",
+        "elasticloadbalancing:DeleteListener",
+        "elasticloadbalancing:DeleteTargetGroup",
+        "elasticloadbalancing:DescribeListeners",
+        "elasticloadbalancing:DescribeLoadBalancerPolicies",
+        "elasticloadbalancing:DescribeTargetGroups",
+        "elasticloadbalancing:DescribeTargetHealth",
+        "elasticloadbalancing:ModifyListener",
+        "elasticloadbalancing:ModifyTargetGroup",
+        "elasticloadbalancing:RegisterTargets",
+        "elasticloadbalancing:SetLoadBalancerPoliciesOfListener",
+        "iam:CreateServiceLinkedRole",
+        "kms:DescribeKey"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource  "aws_iam_instance_profile" "k8s-master" {
+  name = "k8s-master-${var.cluster-name}"
+  role = "${aws_iam_role.k8s-master.name}"
+}
+
+resource "aws_iam_role" "k8s-milpa-worker" {
+  name = "k8s-milpa-worker-${var.cluster-name}"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "k8s-milpa-worker" {
+  name = "k8s-milpa-worker-${var.cluster-name}"
+  role = "${aws_iam_role.k8s-milpa-worker.id}"
   policy = <<EOF
 {
   "Version": "2012-10-17",
@@ -231,9 +328,9 @@ resource "aws_iam_role_policy" "k8s-milpa" {
 EOF
 }
 
-resource  "aws_iam_instance_profile" "k8s-milpa" {
-  name = "k8s-milpa-${var.cluster-name}"
-  role = "${aws_iam_role.k8s-milpa.name}"
+resource  "aws_iam_instance_profile" "k8s-milpa-worker" {
+  name = "k8s-milpa-worker-${var.cluster-name}"
+  role = "${aws_iam_role.k8s-milpa-worker.name}"
 }
 
 data "template_file" "master-userdata" {
@@ -269,7 +366,7 @@ resource "aws_instance" "k8s-master" {
   key_name = "${var.ssh-key-name}"
   associate_public_ip_address = true
   vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
-  iam_instance_profile = "${aws_iam_instance_profile.k8s-milpa.id}"
+  iam_instance_profile = "${aws_iam_instance_profile.k8s-master.id}"
 
   depends_on = ["aws_internet_gateway.gw"]
 
@@ -284,7 +381,7 @@ resource "aws_instance" "k8s-worker" {
   key_name = "${var.ssh-key-name}"
   associate_public_ip_address = true
   vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
-  iam_instance_profile = "${aws_iam_instance_profile.k8s-milpa.id}"
+  iam_instance_profile = "${aws_iam_instance_profile.k8s-milpa-worker.id}"
 
   depends_on = ["aws_internet_gateway.gw"]
 
