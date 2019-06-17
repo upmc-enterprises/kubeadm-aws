b1e3b0ab6e359f8296b3eedf45bab11ac3bd6981
Vilmos Nebehaj <v.nebehaj@gmail.com>
2019-06-17 16:09:13 -0700

    Use latest xenial AMI

--- main.tf
+++ main.tf
@@ -434,8 +434,24 @@ data "template_file" "worker-userdata" {
   }
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical.
+}
+
 resource "aws_instance" "k8s-master" {
-  ami           = "ami-2ef48339"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.medium"
   subnet_id = "${aws_subnet.publicA.id}"
   user_data = "${data.template_file.master-userdata.rendered}"
@@ -450,7 +466,7 @@ resource "aws_instance" "k8s-master" {
 }
 
 resource "aws_instance" "k8s-milpa-worker" {
-  ami           = "ami-2ef48339"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.medium"
   subnet_id = "${aws_subnet.publicA.id}"
   user_data = "${data.template_file.milpa-worker-userdata.rendered}"
@@ -465,7 +481,7 @@ resource "aws_instance" "k8s-milpa-worker" {
 }
 
 resource "aws_instance" "k8s-worker" {
-  ami           = "ami-2ef48339"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.medium"
   subnet_id = "${aws_subnet.publicA.id}"
   user_data = "${data.template_file.worker-userdata.rendered}"
