157a40eda060f282a32e560b737b17e83386ac5a
Vilmos Nebehaj <v.nebehaj@gmail.com>
2019-08-05 15:37:29 -0700

    CLUSTER_NAME is not used anymore in cleanup script

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_vpc" "main" {
     # Remove any leftover instance, security group etc Milpa created. They
     # would prevent terraform from destroying the VPC.
     when    = "destroy"
-    command = "./cleanup-vpc.sh ${self.id} ${var.cluster-name}"
+    command = "./cleanup-vpc.sh ${self.id}"
     interpreter = ["/bin/bash", "-c"]
     environment = {
       "AWS_REGION" = "${var.region}"
@@ -71,7 +71,7 @@ resource "aws_internet_gateway" "gw" {
     # Remove any leftover instance, security group etc Milpa created. They
     # would prevent terraform from destroying the VPC.
     when    = "destroy"
-    command = "./cleanup-vpc.sh ${self.vpc_id} ${var.cluster-name}"
+    command = "./cleanup-vpc.sh ${self.vpc_id}"
     interpreter = ["/bin/bash", "-c"]
     environment = {
       "AWS_REGION" = "${var.region}"
