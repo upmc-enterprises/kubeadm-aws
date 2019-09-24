045f3cfddcd0a487c0efc66f7b4caf708e521431
Vilmos Nebehaj <v.nebehaj@gmail.com>
2019-09-24 10:25:13 -0700

    Fail if curl download fails

--- milpa-worker.sh
+++ milpa-worker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -v
 
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+curl -fL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
